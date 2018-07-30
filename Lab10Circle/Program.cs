bf42a601e7f6560c20aeb2a638a1732583a9816d
Bradley Freestone <bradley.freestone@gmail.com>
2018-07-29 22:51:00 -0400

    second commit

---
+++ Lab10Circle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab10Circle.Properties;
+
+namespace Lab10Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets figure out a circle \nPlease enter in a radius");
+            double radius = double.Parse(Console.ReadLine());
+            var circle = new Circle(radius);
+            
+            Console.WriteLine($"{Circle.formatedCircumference()}");
+            Console.WriteLine($"{Circle.formatedArea()}");
+        }
+    }
+}
---
+++ Lab10Circle/Properties/Circle1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10Circle.Properties
+{
+    class Circle
+    {
+        private double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            double x = 2 * Math.PI * Radius;
+            return x;
+        }
+        public string  CalculateFormattedCircumference()
+        {
+            double formatedCircumference = double.Parse($"{CalculateCircumference()}");
+            return RoundedNumber(formatedCircumference);
+        }
+        public double CalculatedArea()
+            {
+            double x = Math.PI * Radius * Radius;
+            return x;
+            }
+        public double CalculateFormatedArea()
+        {
+            double formatedArea = double.Parse($"{CalculateFormatedArea()}");
+            return formatedArea;
+        }
+        public string RoundedNumber (double x)
+        {
+            return String.Format($"{Math.Round(x, 2)}");
+        }
+    }
+}
